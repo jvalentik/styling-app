9d7212844cd2974a9a7c19e5a918bf72b0c095d3
Jan Valentik <jvalentik@sk.ibm.com>
2017-04-23 22:16:29 +0200

    component styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
+import { AuthorComponent } from './author/author.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteComponent
+    QuoteComponent,
+    AuthorComponent
   ],
   imports: [
     BrowserModule,
