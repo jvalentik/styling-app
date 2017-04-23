22f9fa350680a7b62a4eec943c62b92dcf65ec4c
Jan Valentik <jvalentik@sk.ibm.com>
2017-04-23 21:21:00 +0200

    added components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Goal } from './goal/goal';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  courseGoals = [
+    { title: 'Master Styling', isActive: true},
+    { title: 'Understand animations', isActive: false},
+    { title: 'Master Angular Animations', isActive: false}
+  ] as Array<Goal>;
 }
--- src/app/goal/goal.ts
+++ src/app/goal/goal.ts
@@ -1,3 +1,7 @@
 /**
  * Created by johny on 23/04/2017.
  */
+export interface Goal {
+  title: string;
+  isActive: boolean;
+}
